f6ba37399c23dfc3f8c27b65f8307d257e35a2c7
John2941 <john15509@gmail.com>
2016-03-16 19:09:59 -0500

    added file size

---
+++ easy/file size/main.py
@@ -0,0 +1,19 @@
+"""
+@Project Name - main
+@author - Johnathan
+@date - 3/16/2016
+@time - 7:05 PM
+
+
+"""
+
+import os
+import sys
+
+data_file = os.path.dirname(os.path.realpath(__file__)) + "\\data"
+
+#file = sys.argv[1]
+file = data_file
+
+stats = os.stat(file)
+print stats.st_size
