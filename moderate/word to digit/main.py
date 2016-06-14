b745a885c1d7848683d30e3b3d1b6747de3f9877
Johnathan S <Johnathan S>
2016-06-14 17:47:54 -0400

    update

--- moderate/word to digit/main.py
+++ moderate/word to digit/main.py
@@ -2,9 +2,10 @@
 @Project Name - main
 @author - JOHNATHAN
 @date - 6/14/2016
-@time - 5:07 PM
-@url - https://www.codeeval.com/open_challenges/43/
+@time - 5:35 PM
+@url - https://www.codeeval.com/open_challenges/104/
 """
+
 import os
 import sys
 
@@ -16,15 +17,9 @@ except IOError:
 	with open(sys.argv[1], 'r') as input_file:
 		data = [x.strip('\n') for x in input_file.read().split('\n') if x]
 
-for x in data:
-	array = x.split(' ')
-	array = map(int, array)
-	size = len(array)
-	for index,num in enumerate(array[1:]):
-		absolute = abs(array[index] - array[index - 1])
-		if not 0 < absolute < size:
-			print "Not jolly"
-			break
-	else:
-		print "Jolly"
-
+for line in data:
+	translate = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+	num = ''
+	for word in line.split(';'):
+		num += str(translate[word])
+	print num
